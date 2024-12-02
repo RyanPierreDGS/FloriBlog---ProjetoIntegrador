69d6726c7938082a3578d0a21f43b07987b57e43
Alessandro Moraes <101836197+AlessandroLu@users.noreply.github.com>
2024-12-02 11:19:48 -0400

    adição node -> main

---
+++ js/container_info-second.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const infoPontos = document.querySelectorAll(".box_info-pontos");
+  const boxPonto = document.getElementById("box_ponto");
+
+  infoPontos.forEach((ponto) => {
+    ponto.addEventListener("mouseover", function () {
+      // Limpa o conteúdo anterior
+      boxPonto.innerHTML = "";
+
+      // Cria um novo card
+      const card = document.createElement("div");
+      card.classList.add("card-info");
+      card.innerText = this.getAttribute("data-info");
+
+      // Adiciona o card ao box_ponto
+      boxPonto.appendChild(card);
+    });
+
+    // Limpar o conteúdo ao sair do hover
+    ponto.addEventListener("mouseout", function () {
+      boxPonto.innerHTML = "";
+    });
+  });
+});
